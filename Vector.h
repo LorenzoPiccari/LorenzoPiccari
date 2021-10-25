5a52829d7e166db2a21b9a809718414dd37295a3
Ariclaps <92533038+Ariclaps@users.noreply.github.com>
2021-10-25 09:07:09 +0200

    Add files via upload

---
+++ Vector.h
@@ -0,0 +1,83 @@
+//VECTOR.H
+
+#ifndef Vector_h
+#define Vector_h
+
+class Vector {
+
+public:
+
+	//Constructors
+
+	Vector();
+	Vector(double x, double y, double z);
+    Vector(const Vector& vector);
+
+	//Destructor
+
+	~Vector();
+
+	//-------------------------------------------------------
+
+	//getters
+
+	double x() const { return x_; }
+	double y() const { return y_; }
+	double z() const { return z_; }
+
+	//setters
+
+	void setx(double x) { x_ = x; }
+	void sety(double y) { y_ = y; }
+	void setz(double z) { z_ = z; }
+
+
+	//-------------------------------------------------------
+
+    //Function: MAGNITUDE
+
+	double mag() const;
+
+	//Function: THETA
+
+	double theta() const;
+
+	//Function: PHI
+
+	double phi() const;
+
+	//Function: SCALAR RPODUCT
+
+	double scalarProduct() const;
+
+	//Function: VECTOR PRODUCT
+
+	double vectorProduct() const;
+
+	//Function: ANGLE
+
+	double angle() const;
+
+
+
+
+
+
+
+
+private:
+
+	double x_;
+	double y_;
+	double z_;
+
+
+
+
+
+
+
+};
+
+
+#endif
