e7f2e3c1516fafabbc320aff33ff15613ad44e8e
Lorenzo Piccari <piccari.1921382@studenti.uniroma1.it>
2021-10-25 10:43:13 +0200

    Ho aggiunto l'impl di theta()

--- Vector.cpp
+++ Vector.cpp
@@ -28,31 +28,47 @@ double Vector::mag() const {
 }
 
 //-----------------------------------------------------------------------------------------------
-//x = mag*sin(phi)*cos(theta)
-//y = mag * sin(phi) * sin(theta)
-//z = mag * cos(phi)
-
-//theta = arccos (x/(mag*sin(phi)))
-//phi = arccos (z/mag)
 
 double Vector::phi() const {
 
     double r = sqrt(pow(x_, 2) + pow(y_, 2) + pow(z_, 2));
     double phi = acos(z_ / r);
+    return phi;
 }
 
+//-----------------------------------------------------------------------------------------------
+
+//implementing theta()
 double Vector::theta() const {
+  
+  //I have distinguish the different cases for the definition of the phase
+  //Every case that I've not included in these following statement is already included in the definition of the function "atan" di <cmath>
+   
+    if(x_ < 0. && y_ >= 0.) {
+        double theta = atan(y_/x_) + M_PI;
+        return theta*180/M_PI;
+      
+  }
 
-    double r = sqrt(pow(x_, 2) + pow(y_, 2) + pow(z_, 2));
-    double theta = acos(x_ / (r * sin(r * cos(z_ / r))));
+    else if(x_ < 0. && y_ < 0.) {
+        double theta = atan(y_/x_) - M_PI;
+        return theta*180/M_PI;
+  }
+    
+    else {
+        double theta = atan(y_/x_);
+        return theta*180/M_PI;
+  }
 
-}
+}// closing the implementation of theta()
+
+//---------------------------------------------------------------------------
 
 //implementing SCALAR PRODUCT
 
 //double Vector::scalarProduct() const {
 
-    //double scalarProduct
+}
 
 //---------------------------------------------------------------------------
 
