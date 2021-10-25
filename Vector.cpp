84a36280c125d9ce495993e76763756dcc04f061
Ariclaps <92533038+Ariclaps@users.noreply.github.com>
2021-10-25 10:06:08 +0200

    Add files via upload

---
+++ Vector.cpp
@@ -0,0 +1,55 @@
+
+//implementation file of class Vector
+
+#include "Vector.h"
+#include <iostream>
+#include <cmath>
+
+//constructors
+
+//destructor
+
+Vector::~Vector() {
+    std::cout << "Destroying the Vector object";
+} //I have implemented the destructor with this cout so we can see where it is used at the execution time
+
+//getters and setters have been imlemented in .h as inline functions
+
+//-----------------------------------------------------------------------------------------------
+
+
+//implementing mag()
+
+double Vector::mag() const {
+
+    double mag = sqrt(pow(x_, 2) + pow(y_, 2) + pow(z_, 2));
+    return mag;
+}
+
+//x = mag*sin(phi)*cos(theta)
+//y = mag * sin(phi) * sin(theta)
+//z = mag * cos(phi)
+
+//theta = arccos (x/(mag*sin(phi)))
+//phi = arccos (z/mag)
+
+double Vector::phi() const {
+
+    double r = sqrt(pow(x_, 2) + pow(y_, 2) + pow(z_, 2));
+    double phi = r * cos(z_ / r);
+}
+
+double Vector::theta() const {
+
+    double r = sqrt(pow(x_, 2) + pow(y_, 2) + pow(z_, 2));
+    double theta = acos(x_ / (r * sin(r * cos(z_ / r))));
+
+}
+
+//implementing SCALAR PRODUCT
+
+//double Vector::scalarProduct() const {
+
+    //double scalarProduct
+
+//}
