dced4727a0756d22f3698370a476a4560d81faf1
Lorenzo Piccari <piccari.1921382@studenti.uniroma1.it>
2021-10-25 09:27:35 +0200

    Il costruttore vuoto e quello copia li ho commentati perché non so come trattarli nello stesso modo in cui ho fatto con quello regolare, tanto per adesso non ci servono

--- Vector.h
+++ Vector.h
@@ -3,98 +3,117 @@
 #ifndef Vector_h
 #define Vector_h
 
+#include <cmath>
+
 class Vector {
 
 public:
+    
+    //Constructors
+    //Vector();
+    //Vector(const Vector& vector);
+    
+    //public methods that are used to call the private regular constructor
+    
+    //Spherical coordinates
+    static Vector Spherical(double mag, double theta, double phi) {
+        return Vector(mag*cos(theta)*cos(phi), mag*sin(theta)*sin(phi), mag*cos(phi));
+    }
+    
+    //Cartesian cordinates
+    static Vector Cartesian(double x1, double x2, double x3) {
+        return Vector(x1,x2,x3);
+    }
+    
+    //---------------------------------------------------------------------------
+    
+    //Destructor
+    ~Vector();
+    
+    //---------------------------------------------------------------------------
 
-	//Constructors
+    //getters
 
-	Vector();
-	Vector(double x, double y, double z);
-    Vector(const Vector& vector);
+    double x() const { return x_; }
+    double y() const { return y_; }
+    double z() const { return z_; }
 
-	//Destructor
+    //setters
 
-	~Vector();
-
-	//-------------------------------------------------------
-
-	//getters
-
-	double x() const { return x_; }
-	double y() const { return y_; }
-	double z() const { return z_; }
-
-	//setters
-
-	void setx(double x) { x_ = x; }
-	void sety(double y) { y_ = y; }
-	void setz(double z) { z_ = z; }
+    void setx(double x) { x_ = x; }
+    void sety(double y) { y_ = y; }
+    void setz(double z) { z_ = z; }
 
 
-	//-------------------------------------------------------
-
+    //---------------------------------------------------------------------------
+    
+   
     //Function: MAGNITUDE
 
-	double mag() const;
+    double mag() const;
 
-	//Function: THETA
+    //Function: THETA
 
-	double theta() const;
+    double theta() const;
 
-	//Function: PHI
+    //Function: PHI
 
-	double phi() const;
+    double phi() const;
 
-	//Function: SCALAR RPODUCT
+    //Function: SCALAR RPODUCT
 
-	double scalarProduct() const;
+    double scalarProduct() const;
 
-	//Function: VECTOR PRODUCT
+    //Function: VECTOR PRODUCT
 
-	double vectorProduct() const;
+    double vectorProduct() const;
 
-	//Function: ANGLE
+    //Function: ANGLE
 
-	double angle() const;
+    double angle() const;
 
-	//-------------------------------------------------------
+    //-------------------------------------------------------
 
-	//Operator + (Vector+Vector)
+    //Operator + (Vector+Vector)
 
-	Vector operator+(const Vector& vect) const;
+    Vector operator+(const Vector& vect) const;
 
-	//Operator - (Vector-Vector)
+    //Operator - (Vector-Vector)
 
-	Vector operator-(const Vector& vect) const;
+    Vector operator-(const Vector& vect) const;
 
-	//Operator * (Vector*double) 
-	
-	Vector operator*(const double& vect) const;
+    //Operator * (Vector*double)
+    
+    Vector operator*(const double& vect) const;
 
-	//Operator / (Vector/double)
+    //Operator / (Vector/double)
 
-	Vector operator/(const double& vect) const;
+    Vector operator/(const double& vect) const;
 
-	//Operator =
+    //Operator =
 
-	const Vector& operator=(const Vector& vect);
+    const Vector& operator=(const Vector& vect);
+
 
 
 
 private:
+    double x_,y_,z_;
+    
+    //regular constructor
+    Vector(double x, double y, double z){
+        x = x_;
+        y = y_;
+        z = z_;
+    };
+    
+    
+    
+    
+}; // closing the class
 
-	double x_;
-	double y_;
-	double z_;
+//---------------------------------------------------------------------------
 
+//global functions
 
-
-
-
-
-
-};
-
-
-#endif
+#endif /* Vector_h */
