f5c9667abf04c626659fbfe5200260868e43e8b2
Lorenzo Piccari <piccari.1921382@studenti.uniroma1.it>
2021-10-26 14:46:35 +0200

    Ho corretto qualcosina, tipo in phi() stampavamo il risultato in radianti e non in gradi

--- Vector.cpp
+++ Vector.cpp
@@ -5,17 +5,19 @@
 #include <iostream>
 #include <cmath>
 
-//constructors
+//constructors have been already implemented in the .h
 
 //-----------------------------------------------------------------------------------------------
 
 //destructor
 
 Vector::~Vector() {
-    std::cout << "Destroying the Vector object";
+    std::cout << "\n  Destroying the Vector object  \n";
 } //I have implemented the destructor with this cout so we can see where it is used at the execution time
 
-//getters and setters have been imlemented in .h as inline functions
+//-----------------------------------------------------------------------------------------------
+
+//getters and setters have already been implemented in .h as inline functions
 
 //-----------------------------------------------------------------------------------------------
 
@@ -29,20 +31,23 @@ double Vector::mag() const {
 
 //-----------------------------------------------------------------------------------------------
 
+//implementing phi()
+
 double Vector::phi() const {
 
     double r = sqrt(pow(x_, 2) + pow(y_, 2) + pow(z_, 2));
     double phi = acos(z_ / r);
-    return phi;
+    return phi*180/M_PI;
 }
 
 //-----------------------------------------------------------------------------------------------
 
 //implementing theta()
+
 double Vector::theta() const {
   
-  //I have distinguish the different cases for the definition of the phase
-  //Every case that I've not included in these following statement is already included in the definition of the function "atan" di <cmath>
+  //I have to distinguish the different cases for the definition of the arctan
+  //Every case that I've not included in these following statements is already included in the definition of the function "atan" di <cmath>
    
     if(x_ < 0. && y_ >= 0.) {
         double theta = atan(y_/x_) + M_PI;
@@ -60,7 +65,7 @@ double Vector::theta() const {
         return theta*180/M_PI;
   }
 
-}// closing the implementation of theta()
+}
 
 //---------------------------------------------------------------------------
 
@@ -68,12 +73,12 @@ double Vector::theta() const {
 
 double Vector::scalarProduct(const Vector& vect) const {
 
-    //Product of the two 1° elements
     double Scal = x_ * vect.x_ + y_ * vect.y_ + z_ * vect.z_;
     
     return Scal;
 
 }
+
 
 //---------------------------------------------------------------------------
 
